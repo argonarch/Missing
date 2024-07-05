c88a1d99e20816b4ff4d7c83cc94d6cd6b9184a8
argonarch <armando12123434@gmail.com>
2024-07-05 06:16:47 -0300

    remove seconds, add 0 to nums between 0 to 9

--- src/main/java/com/argonarch/app/App.java
+++ src/main/java/com/argonarch/app/App.java
@@ -1,48 +1,52 @@
 package com.argonarch.app;
+
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.LocalDate;
-import java.time.ZoneId;
+import java.time.LocalDateTime;
 import java.time.Period;
-import java.util.ArrayList; 
-import java.util.List; 
+import java.time.ZoneId;
+
 /**
- * Input String yearsNow, String yearsDead
- * Output time life =  years, months, days, hours, minutes
- *
+ * Input String yearsNow, String yearsDead Output time life = years, months, days, hours, minutes
  */
-public class App 
-{
-  public static void main( String[] args )
-  {	
-		Instant dateNow = Instant.now();
-		LocalDateTime timeNow = LocalDateTime.ofInstant(dateNow, ZoneId.systemDefault());
-		Integer hourInvert = (timeNow.getHour() - 23)*-1;
-		Integer minuteInvert = (timeNow.getMinute() - 59)*-1;
-		Integer secondInvert = (timeNow.getSecond() - 60)*-1;
-		String clockInvert = hourInvert.toString() + ":" + minuteInvert.toString() + ":" + secondInvert.toString();
-		
+public class App {
+  public static void main(String[] args) {
+    String minuteString = "";
+    String hourString = "";
+    Instant dateNow = Instant.now();
+    LocalDateTime timeNow = LocalDateTime.ofInstant(dateNow, ZoneId.systemDefault());
+    Integer hourInvert = (timeNow.getHour() - 23) * -1;
+    Integer minuteInvert = (timeNow.getMinute() - 59) * -1;
+    // Integer secondInvert = (timeNow.getSecond() - 60)*-1;
+    if (minuteInvert >= 0 && minuteInvert <= 9) {
+      minuteString = "0" + minuteInvert.toString();
+    } else {
+      minuteString = minuteInvert.toString();
+    }
+    if (hourInvert >= 0 && hourInvert <= 9) {
+      hourString = "0" + hourInvert.toString();
+    } else {
+      hourString = hourInvert.toString();
+    }
+    String clockInvert = hourString + ":" + minuteString;
+    LocalDate locaDeteNow = LocalDate.now();
+    String dateDead = args[0];
+    LocalDate date = LocalDate.parse(dateDead);
+    Period p = Period.between(locaDeteNow, date);
+    Integer yearInvert = p.getYears();
+    Integer monthInvert = p.getMonths();
+    Integer dayInvert = p.getDays();
+    String dateInvert =
+        yearInvert.toString() + "y " + monthInvert.toString() + "m " + dayInvert.toString() + "d";
 
-		LocalDate locaDeteNow = LocalDate.now();
-		String dateDead = args[0];
-		LocalDate date = LocalDate.parse(dateDead);
-		Period p = Period.between(locaDeteNow, date);
-		Integer yearInvert = p.getYears();
-		Integer monthInvert = p.getMonths();
-		Integer dayInvert = p.getDays();
-		String dateInvert = "Y:" + yearInvert.toString() + " M:" + monthInvert.toString() + " D:" + dayInvert.toString();
-		
+    /* List<Integer> resultList = new ArrayList<Integer>();
+    resultList.add(yearInvert);
+    resultList.add(monthInvert);
+    resultList.add(dayInvert);
+    resultList.add(hourInvert);
+    resultList.add(minuteInvert); */
 
-		
-		List<Integer> resultList = new ArrayList<Integer>();
-		resultList.add(yearInvert);
-		resultList.add(monthInvert);
-		resultList.add(dayInvert);
-		resultList.add(hourInvert);
-		resultList.add(minuteInvert);
-		resultList.add(secondInvert);
-
-		String result = dateInvert + "  " + clockInvert;
-		System.out.println(result);
-	}
+    String result = dateInvert + "  " + clockInvert;
+    System.out.println(result);
+  }
 }
